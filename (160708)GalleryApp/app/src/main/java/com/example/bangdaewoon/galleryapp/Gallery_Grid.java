f1dedb91f6374a2eca8aadcf3384156f91044a28
방대운 <BangDaewoon@bangdaeun-ui-MacBook-Pro.local>
2016-07-09 03:05:53 +0900

    160708 커밋 일일포함

---
+++ (160708)GalleryApp/app/src/main/java/com/example/bangdaewoon/galleryapp/Gallery_Grid.java
@@ -0,0 +1,19 @@
+package com.example.bangdaewoon.galleryapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class Gallery_Grid extends Activity {
+
+    //private Integer[] Blizzard={"Diablo3","Starcrft2","HerosOfTheStrom","OverWatch"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gallery_detail);
+
+
+    }
+}
